c7bc5b2e30ac45e865f5637df86eb0dbbd73f9ac
Ewen_lp <124358937+EwenLP@users.noreply.github.com>
2024-10-21 14:20:00 +0200

    mise à jour de mes infos

---
+++ personnes/ewen/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'nom' => 'Le Pennuisic',
+    'prenom' => 'Ewen',
+    'src' => __DIR__ . '/portrait.webp',
+    'description' => 'Je suis alternant developpeur web chez Camileia. Sur La Rochelle et j édudi à Mediaschool Angoulême.',
+];
