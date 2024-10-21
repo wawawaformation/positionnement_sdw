5ee428018ac1d20727b03b090f97c1683cf97b53
Bjorn Silvester <bjorn.silvester@gmail.com>
2024-10-21 14:37:34 +0200

    mise à jour de mes infos

---
+++ personnes/bjorn/index.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'nom' => 'SILVESTER',
+    'prenom' => 'Bjorn',
+    'src' => __DIR__ . '/IMG_',
+    'description' => 'Je suis étudiant en deuxième année de dev web,
+    j\'ai 19 ans et je suis anglais',
+];
