01b8b7a73ee5b19e32ad78a7fdb9012d4e6e5a07
Drygoods <dow.vigier@laposte.net>
2024-10-21 14:17:17 +0200

    mise à jour de mes infos

---
+++ personnes/dorine/david/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'nom' => 'VIGIER',
+    'prenom' => 'Dorine',
+    'src' => __DIR__ . '/jiraya.jpg',
+    'description' => 'Je suis en reconversion professionnelle pour devenir développeuse web',
+];
